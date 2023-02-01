806d368cd3161c4af6ee9201ffe7ad02617fe8b8
LucasCruz <lca.snf21@uea.edu.br>
2023-02-01 15:57:05 -0400

    feat: dbConnect

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from "express"
 import cors from 'cors'
 import { router } from "./routes.js"
+import { dbConect } from "./database/dbConnect.js"
+import * as dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
-
 app.use(cors())
 app.use(express.json())
 app.use(router)
+dbConect(process.env.URL_DB)
 
 
 
