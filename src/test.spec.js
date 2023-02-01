336411d0e17dbfa8d4b7d6535af61fb4ed1d35b1
LucasCruz <lca.snf21@uea.edu.br>
2023-02-01 15:59:37 -0400

    test: adjust testing

--- src/test.spec.js
+++ src/test.spec.js
@@ -4,6 +4,6 @@ import request  from "supertest";
 describe("Get event", ()=>{
     it("Deve ser possivel ler um evento", async ()=>{
        const response = await request(app).get('/event')
-       console.log(response.status)
+       expect(response.status).toBe(200)
     })
 })
