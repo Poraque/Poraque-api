2d33cde31c687dd65ae04f06ae0bc8b9ede4dff4
LucasCruz <lca.snf21@uea.edu.br>
2023-02-01 15:58:25 -0400

    feat: add functions in routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import { getEventFactory } from "./factories/getEventFactory.js"
 
 const router = Router()
 
@@ -6,10 +7,15 @@ router.get('/',(req, res)=>{
     console.log(req)
     res.status(200).json({'message':'conectado'})})
 
-router.get('/event', (req, res)=>{ res.status(200).json({'message':'deu certo'})
-})
+router.get('/event/:id', (req, res)=> 
+    getEventFactory().handle(req, res))
 
 router.get('/event/:start&:limit', (req, res)=>{
+    console.log(req.params.limit)
+    res.status(200).json({'message':`${req.params.start }, ${req.params.limit}`})
+})
+
+router.get('/hotel/:start&:limit', (req, res)=>{
     console.log(req.params.limit)
     res.status(200).json({'message':`${req.params.start }, ${req.params.limit}`})
 })
