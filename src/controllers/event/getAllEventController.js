8f5d870f5d1547d8cd81aa5accb91fc92b9aefa3
LucasCruz <lca.snf21@uea.edu.br>
2023-02-11 14:42:16 -0400

    refactor: removed unused variable

--- src/controllers/event/getAllEventController.js
+++ src/controllers/event/getAllEventController.js
@@ -1,11 +1,10 @@
 export class GetAllEventCrotroller{
     constructor(getEventUseCase){
-        this.getEventUseCase = getEventUseCase
+        this.getAllEventUseCase = getEventUseCase
     }
 
     async handle(req, res){
-        const id = req.params.id
-        const event = await this.getAllEventUseCase.execute(id)
+        const event = await this.getAllEventUseCase.execute()
         res.status(200).json(event)
     }
 }
