96cd27f87c60a21a18e1dddc9a16802b29862faa
LucasCruz <lca.snf21@uea.edu.br>
2023-02-11 14:47:20 -0400

    perf: limit for sending items added

--- src/usecases/getAllEventUseCase.js
+++ src/usecases/getAllEventUseCase.js
@@ -3,14 +3,29 @@ export class GetAllEventUseCase{
         this.eventRepository = EventRepository
     }
 
-    async execute(start, limit){
-       try{
-       const listEvent = this.eventRepository.getAllEvent(start, limit)
+    async execute(){
+        try{
+            const start = 0
+            const limit = 5 //Limite da tela de home
 
-       return listEvent
-        }catch(err){
-            return {'method':'getEvent',
-                    'error': err}
+            const listEvent = await this.eventRepository.getListEventsByType(start, limit, "Turistico") 
+            const listHotel = await this.eventRepository.getListEventsByType(start, limit, "Hotel")
+            const listFestival = await this.eventRepository.getListEventsByType(start, limit, "Festival")
+            const listFair = await this.eventRepository.getListEventsByType(start, limit, "Feira")
+
+
+            const listAllEvent = listEvent
+            .concat(listHotel)
+            .concat(listFestival)
+            .concat(listFair)
+            
+
+            return listAllEvent
         }
+        catch(err){
+            //Dev
+            return {'method':'getAllEvent',
+                    'error': err}
+            }
     }
 }
