0cf1fdb6ac371154b0177420b4a396accdeeed60
LucasCruz <lca.snf21@uea.edu.br>
2023-02-11 14:38:39 -0400

    test: change route

--- src/test.spec.js
+++ src/test.spec.js
@@ -3,7 +3,8 @@ import request  from "supertest";
 
 describe("Get event", ()=>{
     it("Deve ser possivel ler um evento", async ()=>{
-       const response = await request(app).get('/event')
-       expect(response.status).toBe(200)
+        const response = await request(app).get('/event/all')
+        expect(response.status).toBe(200)
+        expect(response.body[0]).toHaveProperty('_id')
     })
 })
