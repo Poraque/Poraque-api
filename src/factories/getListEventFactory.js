94b92de5b3c89565ea001d7323b6dcc125f59517
LucasCruz <lca.snf21@uea.edu.br>
2023-02-13 10:21:02 -0400

    fix: remove listEvent and listHotel

--- src/factories/getListEventFactory.js
+++
@@ -1,7 +0,0 @@
-export const getHotelFactory = () =>{
-    const eventRepository = new EventRepository()
-    const getListEvenUseCase = new GetListEventUseCase(eventRepository)
-    const getListEventController = new GetListEventCrotroller(getListEvenUseCase)
-
-    return getListEventController
-}
