f5e27c906a83af9b4bd14ac70e73129358c31c85
LucasCruz <lca.snf21@uea.edu.br>
2023-03-28 21:22:55 -0400

    feat: add multer

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,20 @@
 import { Router } from "express"
 import { eventFactory } from "./factories/eventFactory.js"
-
+import multer from "multer";
 const router = Router()
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './src/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "_" + file.originalname)
+  }
+}) 
+const upload = multer({
+    storage:storage 
+  })
+
 
 router.get('/',(req, res)=>{ 
     console.log(req)
@@ -10,13 +23,13 @@ router.get('/',(req, res)=>{
 router.get('/event', (req, res)=> 
     eventFactory().getAllEvents(req, res))
 
-
 router.get('/event/search', (req, res)=>
     eventFactory().searchEvents(req, res))
 
 router.get('/event/:id', (req, res)=> 
     eventFactory().getEventById(req, res))
 
+router.patch('/event/:id', upload.single('event_img'), (req, res)=>
+      eventFactory().editEvent(req, res))
 
 export {router}
-
