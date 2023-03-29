cd9b4a4eef72e851082397293c108f42b3f1f3ef
LucasCruz <lca.snf21@uea.edu.br>
2023-03-28 21:23:06 -0400

    update

--- src/app.js
+++ src/app.js
@@ -3,11 +3,18 @@ import cors from 'cors'
 import { router } from "./routes.js"
 import { dbConect } from "./database/dbConnect.js"
 import * as dotenv from 'dotenv'
+import bodyParser from 'body-parser';
+import multer from 'multer'
+
+
+
+
 
 dotenv.config()
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(bodyParser.json());
 app.use(router)
 dbConect(process.env.URL_DB)
 
